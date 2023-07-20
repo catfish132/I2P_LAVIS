8eae78826940b5ecaf3258aa36959eb636dc9daa
jinyuda132 <212320062@hdu.edu.cn>
2023-07-20 14:34:03 +0800

    修改前端界面

--- app/caption.py
+++ app/caption.py
@@ -23,7 +23,11 @@ from lavis.models import load_model_and_preprocess
 from lavis.processors import load_processor
 from PIL import Image
 
+
 def clean(strs):
+    """
+        用于后处理生成的tags
+    """
     # 将输入字符串按逗号分割成单词列表
     words = strs.split(',')
 
@@ -35,18 +39,31 @@ def clean(strs):
 
     return result
 
+
 def app():
     # ===== layout =====
     device = torch.device("cuda")
     st.markdown(
-        "<h1 style='text-align: center;'>Image Description Generation</h1>",
+        "<h1 style='text-align: center;'>Image to Tags Generation</h1>",
         unsafe_allow_html=True,
     )
+
+    with st.sidebar:
+        st.write("推荐使用maxlen后缀的模型，他们可以生成更长的tags")
+        model_type = st.selectbox(
+            "model type:", ["minicoco_enhanced_maxlen", "3vj_room_maxlen", "3vj_room", "minicoco", "huaban_room",
+                            "minicoco_enhanced_ram"]
+        )
+        st.write("beam_search 会影响推理速度,提升推理质量，选择1即不使用")
+        beam_search = st.selectbox(
+            "beam search", [1, 2, 3, 4, 5]
+        )
 
     instructions = """Try the provided image or upload your own:"""
     file = st.file_uploader(instructions)
 
     col1, col2 = st.columns(2)
+    button_bar, oringal_out = st.columns(2)
 
     if file:
         raw_img = Image.open(file).convert("RGB")
@@ -59,43 +76,59 @@ def app():
     scaling_factor = 720 / w
     resized_image = raw_img.resize((int(w * scaling_factor), int(h * scaling_factor)))
     col1.image(resized_image, use_column_width=True)
-    col2.header("Description")
-    cap_button = st.button("Generate")
-    model, vis_processors = load_model()
+    col2.header("Cleaned Tags")
 
+    with button_bar:
+        st.header("Button Bar")
+        cap_button = st.button("Generate")
+        st.markdown(
+            "<h8> 如果修改了侧边栏的模型类型，请点击'reload_model',以重新加载ckpt</h8>",
+            unsafe_allow_html=True,
+        )
+        reload_model = st.button("reload_model")
+    with oringal_out:
+        st.header("The original output without clean")
+
+    model, vis_processors = init_model()
+
+    if reload_model:
+        model = load(model, model_type)
     if cap_button:
-        # img = vis_processors(raw_img).unsqueeze(0).to(device)
         img = vis_processors["eval"](raw_img).unsqueeze(0).to(device)
         out = model.generate({"image": img},
                              use_nucleus_sampling=False,
-                             num_beams=3,
+                             num_beams=beam_search,
                              max_length=100,
                              min_length=10)
-        # captions = generate_caption(
-        #     model=model, image=img, use_nucleus_sampling=not use_beam
-        # )
         output = clean(out[0])
         col2.write(output, use_column_width=True)
+        oringal_out.write(out[0], use_column_width=True)
 
 
 @st.cache_resource
-def load_model():
+def init_model():
     model, vis_processors, _ = load_model_and_preprocess(name="blip_caption", model_type="blip_img2tag", is_eval=True,
                                                          device=device)
     # model_type选择
-    # 原版模型：large_coco
+    # 原版模型：large_coco 原版只能生成长度有限的tags
     # 长度修改之后：blip_img2tag
-    model.load_checkpoint(
-        '/teams/ai_model_1667305326/WujieAITeam/private/jyd/img2prompt/LAVIS/lavis/output/minicoco_enhanced_maxlen/20230719061/checkpoint_best.pth'
-    )
-    # 3vj_room:"/teams/ai_model_1667305326/WujieAITeam/private/jyd/img2prompt/LAVIS/lavis/output/3vj_Room/20230707100/checkpoint_best.pth"
-    # 3vj_room_maxlen:"/teams/ai_model_1667305326/WujieAITeam/private/jyd/img2prompt/LAVIS/lavis/output/minicoco_enhanced_maxlen/20230719061/checkpoint_best.pth"
-    # minicoco:'/teams/ai_model_1667305326/WujieAITeam/private/jyd/img2prompt/LAVIS/lavis/output/minicoco/20230712072/checkpoint_best.pth'
-    # huaban_room: /teams/ai_model_1667305326/WujieAITeam/private/jyd/img2prompt/LAVIS/lavis/output/Huaban_Room/checkpoint_best.pth
-    # minicoco_enhanced_ram: /teams/ai_model_1667305326/WujieAITeam/private/jyd/img2prompt/LAVIS/lavis/output/minicoco_enhanced/checkpoint_best.pth
-    # minicoco_enhanced_maxlen: /teams/ai_model_1667305326/WujieAITeam/private/jyd/img2prompt/LAVIS/lavis/output/minicoco_enhanced_maxlen/20230719061/checkpoint_best.pth
+    model = load(model)
     model.to(device)
     return model, vis_processors
+
+
+def load(model, model_type="minicoco_enhanced_maxlen"):
+    # 默认使用通用域模型
+    model_map = {  # 模型和路径的映射
+        "3vj_room": "/teams/ai_model_1667305326/WujieAITeam/private/jyd/img2prompt/LAVIS/lavis/output/3vj_Room/20230707100/checkpoint_best.pth",
+        "3vj_room_maxlen": "/teams/ai_model_1667305326/WujieAITeam/private/jyd/img2prompt/LAVIS/lavis/output/3vj_Room_maxlen/20230720014/checkpoint_best.pth",
+        "minicoco": '/teams/ai_model_1667305326/WujieAITeam/private/jyd/img2prompt/LAVIS/lavis/output/minicoco/20230712072/checkpoint_best.pth',
+        "huaban_room": "/teams/ai_model_1667305326/WujieAITeam/private/jyd/img2prompt/LAVIS/lavis/output/Huaban_Room/checkpoint_best.pth",
+        "minicoco_enhanced_ram": "/teams/ai_model_1667305326/WujieAITeam/private/jyd/img2prompt/LAVIS/lavis/output/minicoco_enhanced/checkpoint_best.pth",
+        "minicoco_enhanced_maxlen": "/teams/ai_model_1667305326/WujieAITeam/private/jyd/img2prompt/LAVIS/lavis/output/minicoco_enhanced_maxlen/20230720025/checkpoint_best.pth"
+    }
+    model.load_checkpoint(model_map[model_type])
+    return model
 
 
 # def generate_caption(
